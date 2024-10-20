c2b5ced086c8b23b35d8675863db841f511b03c2
Kelly Ung <kellyung@myyahoo.com>
2024-10-19 21:52:58 -0700

    display map using leaflet

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,8 +1,41 @@
 import './Map.css';
+import { useEffect } from 'react';
+import L from 'leaflet';
+import 'leaflet/dist/leaflet.css';
+import markerIcon from './images/marker.png';
 
 export default function Map() {
+    useEffect(() => {
+        // displays a view of Santa Ana by taking latitude and longitude coordinates 
+        const map = L.map('map').setView([33.7455, -117.8677], 12); 
+    
+        L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            maxZoom: 19,
+            attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+        }).addTo(map);
+        
+        // custom marker icon
+        var myIcon = L.icon({
+            iconUrl: markerIcon,
+            iconSize: [38, 95],
+            iconAnchor: [22, 94],
+            popupAnchor: [-3, -76],
+        });
+        
+        // testing a marker
+        var marker = L.marker([33.7455, -117.8677], {icon: myIcon}).addTo(map);
+        
+        // Cleanup function
+        return () => {
+          map.remove();
+        };
+      }, []);
+
+
 
     return (
-        <h1>Map</h1>
+        <div className="map-page">
+            <div className="map-page" id="map"></div>
+        </div>
     );
 };
