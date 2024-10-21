cfc42f0aaa0233a16ffb3cf42c294c434bf97332
kat-mart <katmartinez@chapman.edu>
2024-10-21 00:05:55 -0700

    adding dropdown search for home page

--- src/components/Explore.js
+++ src/components/Explore.js
@@ -2,6 +2,27 @@ import './Explore.css';
 
 export default function Explore() {
     return (
-        <h1>Explore</h1>
+        <div className="explore-text">
+            <body>
+                <h4>Here are some ways to support</h4>
+                <ul>
+                    <li>Volunteer!</li>
+                    <li>Donate to local your local food pantry</li>
+                        <ul>
+                            <il>Use our search system to get their contact information</il>
+                        </ul>
+                    <li>Suggest additional locations</li>
+                        <ul>
+                            <li>Fill out this form: 
+                                <a 
+                                href="https://example.com/form">
+                                Click here </a>
+                            </li>
+                        </ul>
+                    <li>Review a location</li>
+                </ul>
+            </body>
+        </div>
+
     );
 };
