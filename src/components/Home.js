c675e26aef6912951611fc12f08c88c558e15a4a
kat-mart <katmartinez@chapman.edu>
2024-10-22 15:02:39 -0700

    10/22 updates to filter bar

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,6 +5,7 @@ export default function Home() {
     const [input, setInput] = useState('');
     const [results, setResults] = useState([]);
     const [data, setData] = useState([]);
+    const [filter, setFilter] = useState('');
 
 
     const SHEET_URL = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vSa05FqW_RcHpLANcN1veZxu5z4sHPWsa6_phdBrX7SC0dO5NEcjPWYuJR4qaFIhgk-gEdsHYTyNVFo/pub?output=csv';
@@ -44,14 +45,17 @@ export default function Home() {
     };
     
     const cityFinder = () => {
-        let current;
-        let address;
+        let currentAddress;
+        let currentType;
+        let address = input.toLowerCase();
+        let type = filter.toLowerCase();
         let matches = [];
         for (let i = 0; i < data.length; i++) {
-            current = data.length > 0 ? data[i]["Address"] : 'No data available';
-            current = current.toLowerCase();
-            address = input.toLowerCase();
-            if (current.includes(address)) {
+            currentAddress = data.length > 0 ? data[i]["Address"] : 'No data available';
+            currentType = data.length > 0 ? data[i]["Type"] : 'No data available';
+            currentAddress = currentAddress.toLowerCase();
+            currentType = currentType.toLowerCase();
+            if (currentAddress.includes(address) && (currentType.includes(type))) {
                 matches.push(data[i]);
                 console.log(data[i]);
             }
@@ -62,6 +66,31 @@ export default function Home() {
     const handleSubmit = (e) => {
         e.preventDefault();
         cityFinder();
+    }
+
+    const handleFilter = (option) => {
+        if (option === "Food Pantry") {
+            setFilter("Food Pantry");
+        }
+        else if (option === "Church") {
+            setFilter("Church");
+        }
+        else if (option === "Affordable Grocery") {
+            setFilter("Affordable Grocery");
+        }
+        else if (option === "Food Bank") {
+            setFilter("Food Bank");
+        }
+        else if (option === "Distribution Center") {
+            setFilter("Distribution Center");
+        }
+        else if (option === "Soup Kitchen") {
+            setFilter("Soup Kitchen");
+        }
+        else if (option == "None"){
+            setFilter("");
+        }
+        console.log(filter)
     }
 
     return (
@@ -78,12 +107,16 @@ export default function Home() {
                         placeholder='Enter zip code or city...'
                     />
                  
-                <div class="dropdown">
-                <button class="dropbtn">Assistance Type</button>
-                <div class="dropdown-content">
-                    <button class="save-button" onclick="saveInput()">Food Pantry </button>
-                    <button class="save-button" onclick="saveInput()">Church</button>
-                    <button class="save-button" onclick="saveInput()">Affordable Grocery</button>
+                <div className="dropdown">
+                <button className="dropbtn">{filter !== '' ? filter : "Assistance Type"}</button>
+                <div className="dropdown-content">
+                    <button className="save-button" onClick={() => handleFilter("None")}>None </button>
+                    <button className="save-button" onClick={() => handleFilter("Food Pantry")}>Food Pantry </button>
+                    <button className="save-button" onClick={() => handleFilter("Food Bank")}>Food Bank </button>
+                    <button className="save-button" onClick={() => handleFilter("Church")}>Church </button>
+                    <button className="save-button" onClick={() => handleFilter("Affordable Grocery")}>Affordable Grocery </button>
+                    <button className="save-button" onClick={() => handleFilter("Distribution Center")}>Distribution Center </button>
+                    <button className="save-button" onClick={() => handleFilter("Soup Kitchen")}>Soup Kitchen </button>
                 </div>
                 </div>
 
