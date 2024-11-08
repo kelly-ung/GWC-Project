b922897f93307bfe8826979fe6e2cc4f74020864
Kelly Ung <kellyung@myyahoo.com>
2024-11-08 14:49:19 -0800

    Add useState for mode

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import './App.css';
 import { Route, Routes } from 'react-router-dom';
+import { useState } from 'react';
 import Navbar from './components/Navbar';
 import Home from './components/Home';
 import Map from './components/Map';
 import Suggestions from './components/Suggestions';
 
 export default function App() {
+    const [mode, setMode] = useState(true);
     return (
         <div>
-            <Navbar />
-            <div className='App-header'>
+            <Navbar mode={mode} setMode={setMode} />
+            <div className={mode ? 'App-header-dark' : 'App-header-light'}>
                 <Routes>
                         <Route path='/' element={<Home />} />
                         <Route path='/Home' element={<Home />} />
