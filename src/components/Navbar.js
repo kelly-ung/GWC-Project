15ba8fbfb5c18a177527295037db90c53cdfc6bd
kat-mart <katmartinez@chapman.edu>
2024-11-11 12:50:28 -0800

    updating logo

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,6 @@
 import './Navbar.css';
 import { Link } from 'react-router-dom';
-import logo from './images/orange-logo.png';
+import logo from './images/orangeC.png';
 
 export default function Navbar({ mode, setMode }) {
     const handleClick = () => {
